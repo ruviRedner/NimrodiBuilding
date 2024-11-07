3d5875a0fe0b70d83d6e92971a650092f6995209
ruviRedner <r.y.r7966@gmail.com>
2024-11-07 12:59:57 +0200

    add useEffect

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,37 +1,45 @@
 import { ReactNode, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface IPrivateRoute {
   component: ReactNode;
 }
 
 const PrivateRoute = ({ component }: IPrivateRoute) => {
-  //FILL HERE 3.6
   const { index } = useParams<{ index: string }>();
   const floorIndex = parseInt(index || "0");
+
+  
   const floorAccess = useSelector(
-    (state: {
-      floorAccess: {
-        floorAccess: [boolean, boolean, boolean, boolean, boolean];
-      };
-    }) => state.floorAccess.floorAccess
+    (state: { floorAccess: { floorAccess: [boolean, boolean, boolean, boolean, boolean] } }) =>
+      state.floorAccess.floorAccess
   );
+
   const navigate = useNavigate();
   const [hasAccess, setHasAccess] = useState<boolean>(false);
 
   useEffect(() => {
+    
     if (floorIndex >= 0 && floorIndex < floorAccess.length) {
       setHasAccess(floorAccess[floorIndex]);
     }
   }, [floorAccess, floorIndex]);
 
+  
+  useEffect(() => {
+    if (!hasAccess) {
+      navigate("/forbidden");
+    }
+  }, [hasAccess, navigate]);
+
+  
   if (hasAccess) {
-    return { component };
-  } else {
-    navigate("/forbidden");
-    return null;
+    return <>{component}</>;
   }
+
+  
+  return null;
 };
 
 export default PrivateRoute;
