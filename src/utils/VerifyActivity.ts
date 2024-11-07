e7cc2df649f896adcf463d8c338572e33c79d21a
ruviRedner <r.y.r7966@gmail.com>
2024-11-07 11:18:26 +0200

    custom hook verified done

--- src/utils/VerifyActivity.ts
+++ src/utils/VerifyActivity.ts
@@ -8,6 +8,11 @@ interface IuseActivityParams {
 
 const useIsVerified = ({ activity, role, activities }: IuseActivityParams) => {
   //FILL HERE 3.7
+  const roleIndex = roles.indexOf(role);
+  const activityIndex = activities.indexOf(activity);
+
+  
+  return roleIndex >= activityIndex && activityIndex !== -1;
 };
 
 export default useIsVerified;
