1a6da9a184f823a407f675ced9648476d54c94f4
ruviRedner <r.y.r7966@gmail.com>
2024-11-07 11:46:59 +0200

    private route done

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -2,12 +2,36 @@ import { ReactNode, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 
-interface IPrivateRoute{
-    component: ReactNode,
+interface IPrivateRoute {
+  component: ReactNode;
 }
 
-const PrivateRoute = ({ component}:IPrivateRoute) => {
-    //FILL HERE 3.6
+const PrivateRoute = ({ component }: IPrivateRoute) => {
+  //FILL HERE 3.6
+  const { index } = useParams<{ index: string }>();
+  const floorIndex = parseInt(index || "0");
+  const floorAccess = useSelector(
+    (state: {
+      floorAccess: {
+        floorAccess: [boolean, boolean, boolean, boolean, boolean];
+      };
+    }) => state.floorAccess.floorAccess
+  );
+  const navigate = useNavigate();
+  const [hasAccess, setHasAccess] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (floorIndex >= 0 && floorIndex < floorAccess.length) {
+      setHasAccess(floorAccess[floorIndex]);
+    }
+  }, [floorAccess, floorIndex]);
+
+  if (hasAccess) {
+    return { component };
+  } else {
+    navigate("/forbidden");
+    return null;
+  }
 };
 
-export default PrivateRoute
+export default PrivateRoute;
