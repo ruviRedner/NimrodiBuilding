5ce35f9273c42715ed06ffac33b117980e1c0e84
ruviRedner <r.y.r7966@gmail.com>
2024-11-07 10:38:56 +0200

    role slice done

--- src/store/rolereducer.ts
+++ src/store/rolereducer.ts
@@ -1,1 +1,24 @@
 //FILL HERE 3.2
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+const response = await fetch("../data/roles.json");
+const roles = await response.json();
+
+const initialState = {
+  currentRole: roles[0],
+};
+
+const roleSlice = createSlice({
+  name: "role",
+  initialState,
+  reducers: {
+    setRole: (state, action: PayloadAction<number>) => {
+      const index = action.payload;
+      if (index >= 0 && index < roles.length) {
+        state.currentRole = roles[index];
+      }
+    },
+  },
+});
+
+export const { setRole } = roleSlice.actions;
+export default roleSlice.reducer;
