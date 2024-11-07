201bff366c9c7cfe80a39e6636bdb8c8ca1f7abe
ruviRedner <r.y.r7966@gmail.com>
2024-11-07 10:39:26 +0200

    creating store completed

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,16 @@
 // FILL HERE 3.3
+import { configureStore } from "@reduxjs/toolkit";
+import floorreducer from "../store/floorreducer";
+import rolereducer from "../store/rolereducer";
+
+export const store = configureStore({
+  reducer: {
+    floorAccess: floorreducer,
+    role: rolereducer,
+  },
+});
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
+export default store;
